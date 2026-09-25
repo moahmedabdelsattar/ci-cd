f0b05d425504fd4ba213517a04b12a3f4dd15036
Super User <root@192.168.68.128>
2026-09-25 03:50:56 +0300

    m

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Project 01 - Application is Running! and created by mohamed"
+    return "DevOps Project 01 - Application is Running! and created by mohamed1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
